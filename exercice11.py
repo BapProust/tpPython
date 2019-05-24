5f564a9e8bf9ef01bc9a60d77c2a421222d623c7
BapProust <45763541+BapProust@users.noreply.github.com>
2019-05-24 17:55:42 +0200

    Exercice 11 : Calcul d'intégrale

--- exercice11.py
+++ exercice11.py
@@ -1,5 +1,5 @@
 # Faire un script qui calcule l’intégrale de la fonction y = x^2 par la
-# méthode des rectangles avec x [a,b] et un pas p
+# méthode des rectangles avec x compris entre [a,b] et un pas p
 
 def exercice11():
     while True:
